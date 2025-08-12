36168de5877a0227c5f77b2ede8c173c32babefd
lhj000321 <gothf108@gmail.com>
2025-08-12 14:40:41 +0900

    [hotfix] 긴급 보안 취약점 패치 - XSS 방지

---
+++ src/javaproject/util/SecurityUtil.java
@@ -0,0 +1,7 @@
+package javaproject.util;
+
+public class SecurityUtil {
+    public static String santizeInput(String input){
+        return input == null ? "" : input.replaceAll("[<>\"']", " ")
+    }
+}
